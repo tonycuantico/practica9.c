82502f5f7cbae5b2113fb1ce836b35b20f05218b
tonycuantico <159995704+tonycuantico@users.noreply.github.com>
2024-10-11 13:44:17 -0600

    Programa6a.c

---
+++ Programa6a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+int lista[5] = {10, 8, 5, 8, 7};
+int *ap = lista; //Se declara el apuntador ap
+int indice;
+printf("\tLista\n");
+//Se accede a cada elemento del arreglo haciendo uso del ciclo for
+for (indice = 0 ; indice < 5 ; indice++)
+{
+printf("\nCalificación del alumno %d es %d", indice+1,
+*(ap+indice));
+}
+printf("\n");
+return 0;
+}
