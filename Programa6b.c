1e1e335d5d46adbc4a546f2d1a03c482c93854fe
tonycuantico <159995704+tonycuantico@users.noreply.github.com>
2024-10-11 13:45:40 -0600

    Programa6b.c

---
+++ Programa6b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+int lista[5] = {10, 8, 5, 8, 7};
+int *ap = lista; //Se declara el apuntador ap
+int indice = 0;
+printf("\tLista\n");
+//Se accede a cada elemento del arreglo haciendo uso del ciclo while
+while (indice < 5);
+{
+printf("\nCalificación del alumno %d es %d", indice+1, *(ap+indice));
+indice++;
+}
+printf("\n");
+return 0;
+}
